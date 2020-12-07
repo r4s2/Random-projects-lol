fb5f3abb1622a47c3fbaf2d06076eb29694a5537
rehansha-coder <75597455+rehansha-coder@users.noreply.github.com>
2020-12-07 16:18:56 -0600

    Add files via upload

---
+++ PIL stiff.py
@@ -0,0 +1,24 @@
+from pip._vendor import requests
+spaceShips = requests.get("https://swapi.dev/api/starships/").json()
+ships = []
+
+
+class Starship():
+    def __init__(self, name, pilots):
+        self.name = name
+        self.pilots = pilots
+
+    def printStats(self):
+        print(self.name)
+        for name in self.pilots:
+            print(requests.get(name).json()['name'])
+
+
+for ship in spaceShips['results']:
+    currentShip = len(ships)
+    ships.append('a')
+    ships[currentShip] = Starship(ship['name'], ship['pilots'])
+    ships[currentShip].printStats()
+
+    print("\n")
+    
