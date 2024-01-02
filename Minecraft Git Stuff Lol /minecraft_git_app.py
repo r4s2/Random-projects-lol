951510faae359741ec3d2752640a135802018291
r4s2 <rhn.sha@icloud.com>
2024-01-01 20:51:44 -0500

    pull function for minecraft thing

--- Minecraft Git Stuff Lol /minecraft_git_app.py
+++ Minecraft Git Stuff Lol /minecraft_git_app.py
@@ -4,7 +4,7 @@ from tkinter import filedialog
 
 window = ctk.CTk()
 window.title("Minecraft Commits")
-window.geometry("400x100")
+window.geometry("400x150")
 window.resizable(width=False, height=False)
 ctk.set_default_color_theme("blue")
 ctk.set_appearance_mode("Dark")
@@ -32,7 +32,13 @@ def run_commits(commit_list = commit_list):
             os.system(f"git -C '{item}' push")
             os.system("echo 'commits made!'")
             
-
+def pull_new_info(): 
+    global initial_directory 
+    if len(commit_list) >= 1:
+        for index, item in enumerate(commit_list):
+            os.system(f"git -C '{item}' reset --hard")
+            os.system(f"git -C '{item}' pull")
+            os.system("echo 'commits saved!'")
 
 def insert_username():
     global user
@@ -49,11 +55,12 @@ text_entry = ctk.CTkEntry(window, width=150, placeholder_text="Computer Username
 submit_user = ctk.CTkButton(window, text="Enter Username", command=insert_username)
 select_files = ctk.CTkButton(window, text="add Minecraft files to commit", command=open_file_dialog)
 commit_button = ctk.CTkButton(window, text="commit Minecraft files", command=run_commits)
-
+pull_button = ctk.CTkButton(window, text="pull Minecraft files", command=pull_new_info)
 
 text_entry.grid(row=0,column=0,padx=10)
 submit_user.grid(row=0,column=1,padx=10)
 select_files.grid(row=3,column=0,padx=10)
 commit_button.grid(row=3,column=1,padx=10)
+pull_button.grid(row=4,column=1,padx=10)
 
 window.mainloop()
